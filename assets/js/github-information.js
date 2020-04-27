abdd7ad24962fb9706e013da5e738d844fd989df
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-04-27 15:55:57 +0200

    commit test

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -55,10 +55,17 @@ function fetchGitHubInformation(event) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`
                 );
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(
+                    errorResponse.getResponseHeader("X-RateLimit-Reset") * 1000
+                );
+                $("#gh-user-data").html(
+                    `<h4>Too many requests, please wait until ${resetTime.toLocaleDateString()}.</h4>`
+                );
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
-                    `<h2>Error: ${errorResponse.response}</h2>`
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
                 );
             }
         }
